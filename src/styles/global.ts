9edddae08197cd427a762706513a62cab59cd662
Matthew Finucane <matfin@gmail.com>
2022-02-14 20:59:01 +0000

    feat: kick off style system with styled-components

---
+++ src/styles/global.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  ul {
+    list-style: none;
+  }
+`;
+
+export default GlobalStyle;
---
+++ src/styles/vars.ts
@@ -0,0 +1,10 @@
+import { Breakpoints } from './vars.types';
+
+export const breakpoints: Breakpoints = {
+  sm: 320,
+  md: 768,
+  lg: 1024,
+  xl: 1280,
+  xxl: 1440,
+};
+
